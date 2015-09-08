e116f2b51f011b74e556998fbbaa777148e63ab6
allan <lqc_huli@163.com>
2015-09-08 21:25:34 +0800

    add aop batchdelete

--- goodsync-datacenter/src/main/java/com/huobanplus/goodsync/datacenter/common/Constant.java
+++ goodsync-datacenter/src/main/java/com/huobanplus/goodsync/datacenter/common/Constant.java
@@ -12,4 +12,5 @@ public class Constant {
     public static final String SPEC = "spec";
     public static final String SPEC_VALUE = "specValue";
     public static final String PRODUCT = "product";
+    
 }
